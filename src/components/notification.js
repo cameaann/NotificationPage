ee26f8fcd80977bc092bfea01e4a5e611156c6e6
Anna <cameaann@gmail.com>
2022-12-16 16:57:05 +0200

    added tag with picture

--- src/components/notification.js
+++ src/components/notification.js
@@ -1,27 +1,31 @@
-import Image from 'react-image-webp';
-
 
 export default function Notification(props) {
     let notification = props.value;
 
     return(
-            <div className='notification' >
+            <div className =  {notification.status ==='unread' ? 'notification unread':'notification'} >
 
-                <picture>
-                <Image className='notification__userimage' webp={notification.link}
-                />
-                </picture>
+                <img className='notification__userimage' src={notification.photoSrc} alt=""/>
 
                 <div>
                     <div className="notification__container">
-                        <span className='notification__username'>{ props.person }</span>
-                        <span className='notification__message'>{ notification.text}
-                        <a className='notification__link' href="/">{notification.linkName}</a>
-                        </span>
-                        <div className= { notification.status ==='unread' ? 'unread':'hidden'}></div>
+                        <div>
+                            <span className='notification__username'>{ notification.person }</span>
+                            <span className='notification__text'>{ notification.text}
+                            <a className='notification__link' href="/">{notification.linkName}</a>
+                            </span>
+                        </div>
+
+                        <span className= { notification.status ==='unread' ? 'unreadMark':'hidden'}></span>
                     </div>
                     <div className='notification__date'>{notification.date}</div>
+                    <div className= { notification.message !== undefined ? 'notification__message':'hidden'}>{notification.message}</div>
                 </div>
+                <div  className={ notification.picture !== undefined ? 'picture-box':'hidden'}>
+                    <img src= { notification.picture } className={ notification.picture !== undefined ? 'notification__picture':'hidden'} alt="your img"/>
+                </div>
+
+
 
             </div>
         );
--- src/components/notifications.js
+++ src/components/notifications.js
@@ -6,15 +6,28 @@ export default class Notifications extends React.Component {
 
     render(){
         var notifications = [];
+        var unread = 0;
 
         notifications = Records.map(record => {
+
+            if(record.status ==="unread"){  unread ++;}
             return(
                 <Notification value = { record } key={record.id}/>
             )
-
         })
         return(
-            <div>{ notifications }</div>
+            <div>
+                <div className='tab_container'>
+                    <div className='tab_header'>
+                        <div className='tab_title'>Notifications</div>
+                        <div className= { unread > 0 ? 'newMessagesNumber':'hidden'}>{unread}</div>
+                    </div>
+
+                    <div className='markAllButton'>Mark all as read</div>
+                </div>
+                <div>{ notifications }</div>
+            </div>
+
         )
     }
 
