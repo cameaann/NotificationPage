0ff630c5594260858ea621fcb2702cbe7abd7535
Anna <cameaann@gmail.com>
2022-12-29 16:25:12 +0200

    added responsiveness

--- src/components/notification.js
+++ src/components/notification.js
@@ -12,16 +12,16 @@ export default function Notification(props) {
                         <div>
                             <span className='notification__username'>{ notification.person }</span>
                             <span className='notification__text'>{ notification.text}
-                            <a className='notification__link' href="/">{notification.linkName}</a>
+                                <a className='notification__link' href="/">{notification.linkName}</a>
                             </span>
-                        </div>
+                            <span className= { notification.status ==='unread' ? 'unreadMark':'hidden'}></span>
 
-                        <span className= { notification.status ==='unread' ? 'unreadMark':'hidden'}></span>
+                        </div>
                     </div>
                     <div className='notification__date'>{notification.date}</div>
                     <div className= { notification.message !== undefined ? 'notification__message':'hidden'}>{notification.message}</div>
                 </div>
-                <div  className={ notification.picture !== undefined ? 'picture-box':'hidden'}>
+                <div  className={ notification.picture !== undefined ? 'rcontent-box':'hidden'}>
                     <img src= { notification.picture } className={ notification.picture !== undefined ? 'notification__picture':'hidden'} alt="your img"/>
                 </div>
 
--- src/components/notifications.js
+++ src/components/notifications.js
@@ -3,27 +3,40 @@ import Records from '../assets/notifications_db.json';
 import Notification from './notification';
 
 export default class Notifications extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            records: Records
+        }
+    }
+
+    markAllRead(e){
+        this.setState( this.state.records.map(record => { return record.status ="read"}))
+    }
 
     render(){
         var notifications = [];
         var unread = 0;
-
-        notifications = Records.map(record => {
-
+        
+        notifications = this.state.records.map(record =>{
             if(record.status ==="unread"){  unread ++;}
             return(
                 <Notification value = { record } key={record.id}/>
             )
         })
+
         return(
             <div>
                 <div className='tab_container'>
                     <div className='tab_header'>
                         <div className='tab_title'>Notifications</div>
-                        <div className= { unread > 0 ? 'newMessagesNumber':'hidden'}>{unread}</div>
+                        <div className= 'newMessagesNumber'>{unread}</div>
+                        {/* <div className= { unread > 0 ? 'newMessagesNumber':'hidden'}>{unread}</div> */}
                     </div>
 
-                    <div className='markAllButton'>Mark all as read</div>
+                    <div className='markAllButton' onClick={(e)=>{
+                        this.markAllRead(e);
+                    }}>Mark all as read</div>
                 </div>
                 <div>{ notifications }</div>
             </div>
